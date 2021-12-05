584d0d623b615ee8317ac8c13721b619e39eecb1
stasychbr <stasychbr@mail.ru>
2021-12-05 18:40:13 +0300

    connections build time detection

--- Kirpichenko.Stanislav/lab2/build.sh
+++ Kirpichenko.Stanislav/lab2/build.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-cmake -S src -B build
+conn_files=$(find ./src -name *.cpp -printf "%f\n" | grep "conn_*")
+connections=$(echo $conn_files | sed 's/.cpp//g' | sed 's/conn_//g')
+
+cmake -S src -B build -D INTERACTION_TYPES="$connections"
 
 cd build
 cmake --build .
