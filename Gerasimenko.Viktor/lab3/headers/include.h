632528b13f62d9f611fd56b8054a4ca30eadfa5d
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2021-12-17 22:41:46 +0300

    init include

---
+++ Gerasimenko.Viktor/lab3/headers/include.h
@@ -0,0 +1,13 @@
+#ifndef LAB_3_INCLUDE_H
+#define LAB_3_INCLUDE_H
+
+#include <pthread.h>
+#include <cstddef>
+#include <functional>
+#include <list>
+#include <vector>
+#include <cstdlib>
+#include <chrono>
+#include <iostream>
+
+#endif //LAB_3_INCLUDE_H
